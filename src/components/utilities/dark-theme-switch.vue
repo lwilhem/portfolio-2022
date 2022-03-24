6feb4d704ade228519b55f70f63f0d6471df400e
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-03-24 18:50:59 +0100

    Added Headless UI

--- src/components/utilities/dark-theme-switch.vue
+++ src/components/utilities/dark-theme-switch.vue
@@ -1,22 +1,25 @@
 <script setup>
+  import { Switch } from "@headlessui/vue";
   import { Icon } from "@iconify/vue";
   import { ref } from "vue";
 
-  const checked = ref(true);
+  const enabled = ref(false);
 </script>
 
 <template>
   <div class="flex items-center justify-center align-middle">
     <Icon icon="heroicons-solid:sun" class="w-8 h-8 text-yellow-500 mx-3" />
-    <input type="checkbox" id="switch-mode" v-model="checked" class="hidden" />
-    <label
-      for="switch-mode"
-      class="flex items-center bg-zinc-400 cursor-pointer h-4 w-8 rounded-xl relative"
+    <Switch
+      v-model="enabled"
+      :class="enabled ? 'bg-zinc-900' : 'bg-zinc-700'"
+      class="relative inline-flex items-center h-6 rounded-full w-11"
     >
-      <div
-        class="absolute bg-slate-200 rounded-full w-6 h-6 border-zinc-900 border-2"
-      ></div>
-    </label>
+      <span class="sr-only">Enable notifications</span>
+      <span
+        :class="enabled ? 'translate-x-6' : 'translate-x-1'"
+        class="inline-block w-4 h-4 transform bg-white rounded-full"
+      />
+    </Switch>
     <Icon icon="bi:moon-fill" class="w-6 h-6 text-zinc-900 mx-3" />
   </div>
 </template>
