4dad98f8dd9c3846d47d41fa2b6658ef6cb2bac9
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-03-24 14:43:57 +0100

    :lipstick: navbar && code improvements

--- src/components/navbar-component.vue
+++ src/components/navbar-component.vue
@@ -1,28 +1,32 @@
 <script setup>
   import { CubeTransparentIcon, MoonIcon, SunIcon } from "@heroicons/vue/solid";
+  import { ref } from "vue";
+  import { RouterLink } from "vue-router";
+  import LinkList from "./utilities/NavbarLinkList";
 
-  const linkList = ["Home.", "About.", "Skills.", "Projects.", "Contact."];
+  const linkFormat = ref(LinkList);
 </script>
 
 <template>
   <header class="w-full h-16 fixed flex items-center justify-between px-16">
-    <div class="flex items-center justify-center text-slate-200">
+    <RouterLink to="/" class="flex items-center justify-center text-slate-200">
       <CubeTransparentIcon class="w-8 h-8"></CubeTransparentIcon>
       <h1 class="uppercase font-title font-bold text-2xl">Wilhem.</h1>
-    </div>
+    </RouterLink>
     <div>
       <nav>
-        <a
-          href="#"
-          v-for="link in linkList"
-          :key="link"
+        <RouterLink
+          v-for="link in linkFormat"
           class="text-slate-200 font-hero font-extrabold px-3"
-          >{{ link }}</a
+          :to="link.linkRoute"
         >
+          <span>{{ link.linkName }}</span>
+        </RouterLink>
       </nav>
     </div>
-    <div class="flex">
+    <div class="flex items-center justify-center">
       <MoonIcon class="w-8 h-8 text-slate-200"></MoonIcon>
+      <div class="bg-slate-200 w-8 h-4 mx-2 rounded-full"></div>
       <SunIcon class="w-8 h-8 text-yellow-300"></SunIcon>
     </div>
   </header>
---
+++ src/components/utilities/NavbarLinkList.js
@@ -0,0 +1,24 @@
+const LinkList = [
+  {
+    linkName: "Home.",
+    linkRoute: "/",
+  },
+  {
+    linkName: "About.",
+    linkRoute: "/aboutme",
+  },
+  {
+    linkName: "Skills.",
+    linkRoute: "/skills",
+  },
+  {
+    linkName: "Portfolio.",
+    linkRoute: "/portfolio",
+  },
+  {
+    linkName: "Contact.",
+    linkRoute: "/contactme",
+  },
+];
+
+export default LinkList;
