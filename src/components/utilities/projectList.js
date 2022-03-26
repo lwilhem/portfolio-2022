f30562d85028ac988b9e6b3cc8d4e856c756258c
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-03-26 12:50:11 +0100

    :hammer: Improved Project Data Structures

---
+++ src/components/utilities/projectList.js
@@ -0,0 +1,72 @@
+export const projectList = [
+  {
+    id: 0,
+    name: "Project Devlab: L'Amour des Goodies",
+    technologies: [
+      "nestjs",
+      "vuejs",
+      "stripe",
+      "mySQL",
+      "tailwindcss",
+      "vite",
+      "typescript",
+    ],
+    repoLink: [
+      {
+        backend: "https://github.com/lwilhem/devlab-goodies-backend",
+        frontend: "https://github.com/lwilhem/vite-devlab-goodies-client",
+      },
+    ],
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas interdum tortor mi. Vivamus non metus vel leo suscipit suscipit suscipit sit amet massa. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed sed pulvinar elit. Aliquam vitae aliquet libero, ac dignissim libero. Duis mattis nibh tortor, vitae sodales risus euismod non. Cras sed lobortis lacus. Sed sit amet elementum risus. Pellentesque varius auctor fermentum. Mauris id tempus tortor. Vestibulum hendrerit metus in mauris porta venenatis. Phasellus porta tincidunt nibh eu tempus. Sed tristique vitae tellus ut bibendum. Mauris interdum metus ut magna condimentum consequat.",
+  },
+  {
+    id: 1,
+    name: "Symfony Course: Site E-Commerce",
+    technologies: ["symfony", "twig", "bootstrap", "mySQL"],
+    repoLink: [
+      {
+        backend: "https://github.com/lwilhem/devlab-goodies-backend",
+      },
+    ],
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas interdum tortor mi. Vivamus non metus vel leo suscipit suscipit suscipit sit amet massa. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed sed pulvinar elit. Aliquam vitae aliquet libero, ac dignissim libero. Duis mattis nibh tortor, vitae sodales risus euismod non. Cras sed lobortis lacus. Sed sit amet elementum risus. Pellentesque varius auctor fermentum. Mauris id tempus tortor. Vestibulum hendrerit metus in mauris porta venenatis. Phasellus porta tincidunt nibh eu tempus. Sed tristique vitae tellus ut bibendum. Mauris interdum metus ut magna condimentum consequat.",
+  },
+  {
+    id: 2,
+    name: "OOP Course: SPA Registration Site",
+    technologies: ["php", "bootsrap", "html", "css"],
+    repoLink: [
+      {
+        backend: "https://github.com/lwilhem/devlab-goodies-backend",
+      },
+    ],
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas interdum tortor mi. Vivamus non metus vel leo suscipit suscipit suscipit sit amet massa. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed sed pulvinar elit. Aliquam vitae aliquet libero, ac dignissim libero. Duis mattis nibh tortor, vitae sodales risus euismod non. Cras sed lobortis lacus. Sed sit amet elementum risus. Pellentesque varius auctor fermentum. Mauris id tempus tortor. Vestibulum hendrerit metus in mauris porta venenatis. Phasellus porta tincidunt nibh eu tempus. Sed tristique vitae tellus ut bibendum. Mauris interdum metus ut magna condimentum consequat.",
+  },
+  {
+    id: 3,
+    name: "Cours Node: Service de Messagerie Instantanée",
+    technologies: ["node", "express", "vuejs", "mongoDB"],
+    repoLink: [
+      {
+        backend: "https://github.com/lwilhem/devlab-goodies-backend",
+        frontend: "https://github.com/lwilhem/vite-devlab-goodies-client",
+      },
+    ],
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas interdum tortor mi. Vivamus non metus vel leo suscipit suscipit suscipit sit amet massa. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed sed pulvinar elit. Aliquam vitae aliquet libero, ac dignissim libero. Duis mattis nibh tortor, vitae sodales risus euismod non. Cras sed lobortis lacus. Sed sit amet elementum risus. Pellentesque varius auctor fermentum. Mauris id tempus tortor. Vestibulum hendrerit metus in mauris porta venenatis. Phasellus porta tincidunt nibh eu tempus. Sed tristique vitae tellus ut bibendum. Mauris interdum metus ut magna condimentum consequat.",
+  },
+  {
+    id: 4,
+    name: "Portfolio version 2022",
+    technologies: ["vuejs", "tailwind", "vite"],
+    repoLink: [
+      {
+        frontend: "https://github.com/lwilhem/vite-devlab-goodies-client",
+      },
+    ],
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas interdum tortor mi. Vivamus non metus vel leo suscipit suscipit suscipit sit amet massa. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed sed pulvinar elit. Aliquam vitae aliquet libero, ac dignissim libero. Duis mattis nibh tortor, vitae sodales risus euismod non. Cras sed lobortis lacus. Sed sit amet elementum risus. Pellentesque varius auctor fermentum. Mauris id tempus tortor. Vestibulum hendrerit metus in mauris porta venenatis. Phasellus porta tincidunt nibh eu tempus. Sed tristique vitae tellus ut bibendum. Mauris interdum metus ut magna condimentum consequat.",
+  },
+];
--- src/views/ProjectListView.vue
+++ src/views/ProjectListView.vue
@@ -1,79 +1,8 @@
 <script setup>
-  import { Icon } from "@iconify/vue";
   import { ref } from "vue";
-  const projectList = ref([
-    {
-      id: 0,
-      name: "SPA animal Register",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nulla commodo ipsum leo. Praesent arcu mauris, malesuada quis augue a, rutrum mattis leo. Praesent id viverra libero. Duis tempus tristique dui, vitae blandit turpis lacinia in. Etiam vel justo pretium, sodales mi congue, iaculis quam. Maecenas ut nisl ac leo pharetra volutpat nec eget arcu. Nullam mauris ligula, pulvinar quis tempor vitae, vehicula a lorem. Vivamus velit ex, semper eget ex id, tempus maximus tortor.",
-      technologies: [
-        "akar-icons:php-fill",
-        "akar-icons:html-fill",
-        "akar-icons:css-fill",
-        "akar-icons:bootstrap-fill",
-      ],
-      repoLink: "https://github.com/lwilhem/spa-project",
-    },
-    {
-      id: 1,
-      name: "Node Messaging Api",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nulla commodo ipsum leo. Praesent arcu mauris, malesuada quis augue a, rutrum mattis leo. Praesent id viverra libero. Duis tempus tristique dui, vitae blandit turpis lacinia in. Etiam vel justo pretium, sodales mi congue, iaculis quam. Maecenas ut nisl ac leo pharetra volutpat nec eget arcu. Nullam mauris ligula, pulvinar quis tempor vitae, vehicula a lorem. Vivamus velit ex, semper eget ex id, tempus maximus tortor.",
-      technologies: ["akar-icons:node-fill", "logos:express", "ri:vuejs-fill"],
-      repoLink: "https://github.com/lwilhem/node-instant-messaging-api",
-    },
-    {
-      id: 2,
-      name: "Portfolio",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nulla commodo ipsum leo. Praesent arcu mauris, malesuada quis augue a, rutrum mattis leo. Praesent id viverra libero. Duis tempus tristique dui, vitae blandit turpis lacinia in. Etiam vel justo pretium, sodales mi congue, iaculis quam. Maecenas ut nisl ac leo pharetra volutpat nec eget arcu. Nullam mauris ligula, pulvinar quis tempor vitae, vehicula a lorem. Vivamus velit ex, semper eget ex id, tempus maximus tortor.",
-      technologies: [
-        "ri:vuejs-fill",
-        "file-icons:tailwind",
-        "simple-icons:vite",
-      ],
-      repoLink: "https://github.com/lwilhem/spa-project",
-    },
-    {
-      id: 3,
-      name: "Devlab: E-Commerce Client",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nulla commodo ipsum leo. Praesent arcu mauris, malesuada quis augue a, rutrum mattis leo. Praesent id viverra libero. Duis tempus tristique dui, vitae blandit turpis lacinia in. Etiam vel justo pretium, sodales mi congue, iaculis quam. Maecenas ut nisl ac leo pharetra volutpat nec eget arcu. Nullam mauris ligula, pulvinar quis tempor vitae, vehicula a lorem. Vivamus velit ex, semper eget ex id, tempus maximus tortor.",
-      technologies: [
-        "ri:vuejs-fill",
-        "file-icons:tailwind",
-        "simple-icons:vite",
-      ],
-      repoLink: "https://github.com/lwilhem/vite-devlab-goodies-client",
-    },
-    {
-      id: 4,
-      name: "Devlab: E-Commerce Backend",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nulla commodo ipsum leo. Praesent arcu mauris, malesuada quis augue a, rutrum mattis leo. Praesent id viverra libero. Duis tempus tristique dui, vitae blandit turpis lacinia in. Etiam vel justo pretium, sodales mi congue, iaculis quam. Maecenas ut nisl ac leo pharetra volutpat nec eget arcu. Nullam mauris ligula, pulvinar quis tempor vitae, vehicula a lorem. Vivamus velit ex, semper eget ex id, tempus maximus tortor.",
-      technologies: [
-        "file-icons:nestjs",
-        "akar-icons:node-fill",
-        "file-icons:typescript-alt",
-        "fa:cc-stripe",
-      ],
-      repoLink: "https://github.com/lwilhem/vite-devlab-goodies-client",
-    },
-    {
-      id: 5,
-      name: "E-Commerce Fullstack",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nulla commodo ipsum leo. Praesent arcu mauris, malesuada quis augue a, rutrum mattis leo. Praesent id viverra libero. Duis tempus tristique dui, vitae blandit turpis lacinia in. Etiam vel justo pretium, sodales mi congue, iaculis quam. Maecenas ut nisl ac leo pharetra volutpat nec eget arcu. Nullam mauris ligula, pulvinar quis tempor vitae, vehicula a lorem. Vivamus velit ex, semper eget ex id, tempus maximus tortor.",
-      technologies: [
-        "akar-icons:php-fill",
-        "logos:symfony",
-        "akar-icons:bootstrap-fill",
-        "file-icons:twig",
-      ],
-      repoLink: "https://github.com/lwilhem/vite-devlab-goodies-client",
-    },
-  ]);
+  import projectList from "../components/utilities/projectList";
+
+  const formatedList = ref(projectList);
 </script>
 
 <template>
@@ -85,12 +14,7 @@
     >
       What I've been cooking !
     </h2>
-    <div class="flex flex-wrap">
-      <div v-for="project in projectList" :id="project.id">
-        <span>{{ project.name }}</span>
-        <Icon v-for="technology in project.technologies" :icon="technology" />
-      </div>
-    </div>
+    <div class="flex flex-wrap"></div>
   </section>
 </template>
 
