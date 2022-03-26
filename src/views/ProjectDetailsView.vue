2f2f62f0aab74c86cead4cf147bab4cd202b7bbc
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-03-26 21:57:36 +0100

    :construction: WIP ProjectDetails UI

--- src/views/ProjectDetailsView.vue
+++ src/views/ProjectDetailsView.vue
@@ -40,3 +40,15 @@
 </template>
 
 <style></style>
+
+<!--     <p>{{ projectDetails.description }}</p>
+    <div>
+      <Icon v-for="tech in projectDetails.technologies" :icon="tech" />
+    </div>
+    <section>
+      <div v-for="link in projectDetails.repoLink">
+        <a v-if="link.backend" :href="link.backend"> Repo Back</a>
+        <a v-if="link.frontend" :href="link.frontend"> Repo Front</a>
+        <a v-if="link.deployedAt" :href="link.deployedAt">Project Link</a>
+      </div>
+    </section> -->
